18a1466aac1d931dc9f6c8f2e8bb4b95620ff8dc
Enes Celik <enes.celik.41@hotmail.com>
2018-12-28 21:07:01 +0300

    IPublishedProductDal ve UnitTest Yazıldı.

---
+++ Esem/UnitTest/DataAccessTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Business;
+using DataAccess.Abstract;
+using Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class DataAccessTest
+    {
+        [TestMethod]
+        public void FillDatabase()
+        {
+            Account ac = new Account();
+            ac.Username = "eecelik";
+            ac.Firstname = "Enes";
+            ac.Lastname = "Celik";
+            ac.Password = "123";
+            ac.Mail = "iletisim@eecelik.com";
+
+            IAccountDal accountDal = IocUtil.Resolve<IAccountDal>();
+
+            accountDal.Add(ac);
+            
+            
+        }
+    }
+}
