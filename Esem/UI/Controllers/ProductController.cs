b0e534c907157e1f99682a90bee9dd7b29fba651
Enes Celik <enes.celik.41@hotmail.com>
2018-12-29 00:54:49 +0300

    ProductController yazıldı.

---
+++ Esem/UI/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Business.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index(Product product)
+        {
+            IProductService productService = Business.IocUtil.Resolve<IProductService>();
+            string userName = User.Identity.Name;
+            productService.Add(userName,product);
+
+            return View("Index");
+        }
+    }
+}
