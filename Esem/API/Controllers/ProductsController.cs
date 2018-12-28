aae51e85077636c177750b7c6d545695a63c1863
Ersoy TORAMAN <jovannijares@gmail.com>
2018-12-28 20:10:13 +0300

    ProductManager oluşturuldu.

---
+++ Esem/API/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET api/products
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ Esem/Business/Concrete/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
