0f5ff3a432b20ffdec64388cfca0a16d8f1936c0
Ersoy TORAMAN <jovannijares@gmail.com>
2018-12-28 19:12:17 +0300

    LoginManager tamamlandı.

--- Esem/Business/Concrete/LoginManager.cs
+++ Esem/Business/Concrete/LoginManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +11,32 @@ namespace Business.Concrete
 {
     public class LoginManager : ILoginService
     {
+        private IAccountDal accountDal;
+
+        public LoginManager(IAccountDal accountDal)
+        {
+            this.accountDal = accountDal;
+        }
+
         public bool Login(string username, string password)
         {
-            throw new NotImplementedException();
+            Account account = accountDal.Get(x => x.Username == username);
+
+            if (account == null) return false;
+
+            if (account.Password == password) return true;
+            else return false;
         }
 
         public bool Register(Account account)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(account.Firstname.Trim()) || string.IsNullOrEmpty(account.Lastname.Trim()) || string.IsNullOrEmpty(account.Username.Trim()) || string.IsNullOrEmpty(account.Password.Trim()) || string.IsNullOrEmpty(account.Mail.Trim()))
+                return false;
+            
+            //user kontrolleri yapılacak.
+
+            accountDal.Add(account);
+            return true;
         }
     }
 }
