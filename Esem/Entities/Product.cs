b0dc511a192679bacf521f50506a3a7847ef6a5f
Enes Celik <enes.celik.41@hotmail.com>
2018-12-28 19:15:37 +0300

    Product, IProductDal, ProductDal oluşturuldu.

---
+++ Esem/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Product : IEntity
+    {
+        public string Price { get; set; }
+        public byte[] Image { get; set; }
+        public string Description { get; set; }
+        public Account Account { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Address { get; set; }
+    }
+}
