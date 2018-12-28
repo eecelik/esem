a831915a1a7ccd72abaea7b7c4606c250d988c78
Enes Celik <enes.celik.41@hotmail.com>
2018-12-28 20:45:57 +0300

    Product, Category, Account Read-Only yapıldı.

--- Esem/Entities/Product.cs
+++ Esem/Entities/Product.cs
@@ -9,13 +9,13 @@ namespace Entities
 {
     public class Product : IEntity
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public string Price { get; set; }
         public byte[] Image { get; set; }
         public string Description { get; set; }
-        public Account Account { get; set; }
+        public int AccountId { get; set; }
         public string City { get; set; }
         public string District { get; set; }
         public string Address { get; set; }
