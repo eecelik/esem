829f73338a0671bb944205dd8ef8ef6bb6d031d7
Enes Celik <enes.celik.41@hotmail.com>
2018-12-29 06:24:16 +0300

    ISaveProductDal, SaveProductDal, SaveProduct Eklendi.

---
+++ Esem/Entities/SaveProduct.cs
@@ -0,0 +1,16 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class SaveProduct : IEntity
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int AccountId { get; set; }
+    }
+}
